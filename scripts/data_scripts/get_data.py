d7e150f89000b0a66f1eee8a6105945a1ddaadab
Basserti <bassertils@gmail.com>
2023-10-18 16:26:26 +0500

    Fix get_data.py

--- scripts/data_scripts/get_data.py
+++ scripts/data_scripts/get_data.py
@@ -2,12 +2,12 @@
 
 import pandas as pd
 
-df_train = pd.read_csv('/home/user1/project_mlops/MLOPS_course/data/backup_data_not_used/train.csv')
-df_test = pd.read_csv('/home/user1/project_mlops/MLOPS_course/data/backup_data_not_used/test.csv')
-df_macro = pd.read_csv('/home/user1/project_mlops/MLOPS_course/data/backup_data_not_used/macro.csv')
-submission = pd.read_csv('/home/user1/project_mlops/MLOPS_course/data/backup_data_not_used/submission.csv')
+df_train = pd.read_csv('data/backup_data_not_used/train.csv')
+df_test = pd.read_csv('data/backup_data_not_used/test.csv')
+df_macro = pd.read_csv('data/backup_data_not_used/macro.csv')
+submission = pd.read_csv('data/backup_data_not_used/submission.csv')
 
-df_train.to_csv('/home/user1/project_mlops/MLOPS_course/data/raw/train.csv', index=False)
-df_test.to_csv('/home/user1/project_mlops/MLOPS_course/data/raw/test.csv', index=False)
-df_macro.to_csv('/home/user1/project_mlops/MLOPS_course/data/raw/macro.csv', index=False)
-submission.to_csv('/home/user1/project_mlops/MLOPS_course/data/raw/submission.csv', index=False)
+df_train.to_csv('data/raw/train.csv', index=False)
+df_test.to_csv('data/raw/test.csv', index=False)
+df_macro.to_csv('data/raw/macro.csv', index=False)
+submission.to_csv('data/raw/submission.csv', index=False)
