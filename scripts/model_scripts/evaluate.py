4b79c9d2b31615dd69bfda7d152f8c187e20cb12
Basserti <bassertils@gmail.com>
2023-10-18 22:42:55 +0500

    Evaluation of model

--- scripts/model_scripts/evaluate.py
+++ scripts/model_scripts/evaluate.py
@@ -4,9 +4,11 @@ import numpy as np
 import pickle
 import yaml
 import joblib
+import json
 
+from catboost import CatBoostRegressor
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 df_train = pd.read_csv('data/baselines/stage4/train.csv')
 df_test = pd.read_csv('data/baselines/stage4/test.csv')
@@ -21,20 +23,13 @@ random_state = params_tts["random_state"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-params = yaml.safe_load(open("params.yaml"))["train"]
-cat_features = params["cat_features"]
-learning_rate = params["learning_rate"]
-iterations = params["iterations"]
-loss_function = params["loss_function"]
-random_seed = params["seed"]
+model = joblib.load('data/final/model.pkl')
 
-from catboost import CatBoostRegressor
+y_pred = model.predict(X_test)
 
-model = CatBoostRegressor(cat_features=cat_features, learning_rate=learning_rate,iterations=iterations,loss_function=loss_function,random_seed=random_seed)
-print("start fit")
-model.fit(X,y,verbose=0)
-print("end fit")
-os.mkdir("data/final")
+mae = mean_absolute_error(y_test, y_pred)
+print(mae)
+output = {"score": mae} 
 
-with open ('data/final/model.pkl', 'wb') as file:
-    joblib.dump(model, file)
+with open(f"score/evaluate.json", "w") as write_file:
+            json.dump(output, write_file)
