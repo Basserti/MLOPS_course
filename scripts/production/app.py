8d0d5c8bf02756f279977e5fa70c32ce6c6669a2
Basserti <51204419+Basserti@users.noreply.github.com>
2023-12-22 23:33:09 +0500

    Update app.py

--- scripts/production/app.py
+++ scripts/production/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
   print('Hello')
 
-@app.route("/predict/<float:full_sq>/<float:life_sq>/<float:kitch_sq>/<float:num_room>/<float:floor>/<float:max_floor>/<float:state>/<float:material>/<float:build_year>/<int:full_all>/<object:sub_area>/<float:salary>/<float:fixed_basket>/<float:rent_price_1room_eco>/<float:rent_price_2room_eco>/<float:rent_price_3room_eco>/<float:average_life_exp>/")
+@app.route("/predict/<float:full_sq>/<float:life_sq>/<float:kitch_sq>/<float:num_room>/<float:floor>/<float:max_floor>/<float:state>/<float:material>/<float:build_year>/<int:full_all>/<str:sub_area>/<float:salary>/<float:fixed_basket>/<float:rent_price_1room_eco>/<float:rent_price_2room_eco>/<float:rent_price_3room_eco>/<float:average_life_exp>/")
 def predict(full_sq,life_sq,kitch_sq,num_room,floor,max_floor,state,material,build_year,full_all,sub_area,salary,fixed_basket,rent_price_1room_eco,rent_price_2room_eco,rent_price_3room_eco,average_life_exp):
   model = joblib.load('model.pkl')
   y_pred = model.predict([[full_sq,life_sq,kitch_sq,num_room,floor,max_floor,state,material,build_year,full_all,sub_area,salary,fixed_basket,rent_price_1room_eco,rent_price_2room_eco,rent_price_3room_eco,average_life_exp]])[0]
