c37aff95b78e85294750ab904ef43e99a5ba5d96
Basserti <51204419+Basserti@users.noreply.github.com>
2023-12-22 21:50:06 +0500

    Update test1.py

--- scripts/tests/test1.py
+++ scripts/tests/test1.py
@@ -13,7 +13,7 @@ X = df_train.drop(('price_doc'), axis=1)
 X = X.drop(('id'), axis=1)
 y = df_train['price_doc']
 
-params_tts = yaml.safe_load(open("/home/user/git/data/params.yaml"))["split"]
+params_tts = yaml.safe_load(open("/home/user/git/params.yaml"))["split"]
 test_size = params_tts["test_size"]
 random_state = params_tts["random_state"]
 
